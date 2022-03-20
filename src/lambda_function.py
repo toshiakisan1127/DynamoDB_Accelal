c8687f0585e6e9ad5efbf3254b131453a56e84d4
toshiakisan1127 <toshiakisan1127@gmail.com>
2022-03-20 17:30:43 +0900

    add decolator to estimate process time

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,6 +1,9 @@
+from functools import wraps
+import time
 import pytz
 import logging
 import json
+import boto3
 
 from datetime import datetime
 
@@ -8,21 +11,116 @@ logger = logging.getLogger('lambda_logger')
 logger.setLevel(logging.INFO)
 
 
+def stop_watch(func):
+    @wraps(func)
+    def wrapper(*args, **kargs):
+        start = time.time()
+        result = func(*args, **kargs)
+        elapsed_time = time.time() - start
+        print(f"{func.__name__}は{elapsed_time}秒かかりました")
+        return result
+    return wrapper
+
+
+@stop_watch
 def lambda_handler(event: dict, context):
     logger.info('function = %s, version = %s, request_id = %s',
                 context.function_name, context.function_version, context.aws_request_id)
     logger.info('event = %s', event)
+
+    dynamodb_client = DynamodbClient()
+    table_name = "dax_test_table"
     UTC = pytz.timezone('UTC')
     now = datetime.now(UTC)
     timestamp = now.timestamp()
-    return build_response(200, timestamp)
+
+    # N回put
+    N = 5
+    for i in range(N):
+        item = {
+            "user_id": str(i),
+            "test": "hoge"
+        }
+        try:
+            dynamodb_client.put_item(table_name=table_name, item=item)
+        except Exception as e:
+            logger.error("dynamodbへのput操作でエラーが発生しました。: {}".format(e))
+            return ApiGatewayResponse.build_response(500, timestamp)
+        logger.info("put item : {}".format(item))
+
+    # N回getをM回くりかえす
+    M = 2
+    for _ in range(M):
+        for i in range(N):
+            key = {
+                "UserId": {"S": str(i)}
+            }
+            try:
+                item = dynamodb_client.get_item(table_name=table_name, key=key)
+            except Exception as e:
+                logger.error("dynamodbへのget操作でエラーが発生しました。: {}".format(e))
+                return ApiGatewayResponse.build_response(500, timestamp)
+            logger.info("get item : {}".format(item))
+
+    # N回消す
+    for i in range(N):
+        key = {
+            "UserId": {"S": str(i)}
+        }
+        try:
+            deleted_item = dynamodb_client.delete_item(
+                table_name=table_name,
+                key=key
+            )
+        except Exception as e:
+            logger.error("dynamodbへのdelete操作でエラーが発生しました。: {}".format(e))
+            return ApiGatewayResponse.build_response(500, timestamp)
+        logger.info("deleted item : {}".format(deleted_item))
+
+    return ApiGatewayResponse.build_response(200, timestamp)
 
 
-def build_response(code: int, timestamp):
-    responseBody = {"time": timestamp}
-    return {
-        'isBase64Encoded': False,
-        'statusCode': code,
-        'headers': {},
-        'body': json.dumps(responseBody)
-    }
+class DynamodbClient:
+
+    def __init__(self):
+        self.client = boto3.client("dynamodb")
+
+    def put_item(self, table_name: str, item: dict):
+        dynamodb_item = self._dict_to_dynamodb_item_for_dax_test_table(item)
+        self.client.put_item(
+            TableName=table_name,
+            Item=dynamodb_item
+        )
+        return None
+
+    def get_item(self, table_name: str, key: dict):
+        item = self.client.get_item(
+            TableName=table_name,
+            Key=key
+        )
+        return item
+
+    def delete_item(self, table_name: str, key: dict):
+        return self.client.delete_item(
+            TableName=table_name,
+            Key=key
+        )
+
+    def _dict_to_dynamodb_item_for_dax_test_table(self, item: dict):
+        return {
+            "UserId": {"S": item["user_id"]},
+            "test": {"S": item["test"]}
+        }
+
+
+class ApiGatewayResponse:
+
+    @staticmethod
+    def build_response(code: int, timestamp):
+        responseBody = {"time": timestamp}
+        return {
+            'isBase64Encoded': False,
+            'statusCode': code,
+            'headers': {},
+            'body': json.dumps(responseBody)
+        }
